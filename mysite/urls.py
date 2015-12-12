596dbaf544afd4da91e8921d28ffffdc174f5f3b
Wes Basinger <wbasinger@villagetechschools.org>
2015-12-12 16:10:13 -0600

    Added user authentication.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 ]
